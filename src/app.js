8bc83b0cd658bad2dc1dce0d7e9b1bdad8a81c00
sgigli <simon.luciano@gigli.net>
2020-07-23 22:45:54 -0400

    integrating redux

--- src/app.js
+++ src/app.js
@@ -1,7 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter'
+import configureStore from './store/configureStore'
+import { addExpense } from './actions/expenses'
+import { setTextFilter } from './actions/filters'
+import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'))
+const store = configureStore()
+
+store.subscribe(() => {
+    const state = store.getState()
+    const expenses = getVisibleExpenses(state.expenses, state.filters)
+    console.log(expenses)
+})
+
+store.dispatch(addExpense({ description: 'Water Bill', amount: 4500 }))
+store.dispatch(addExpense({ description: 'Gas Bill' }))
+
+store.dispatch(setTextFilter('bill'))
+store.dispatch(setTextFilter('water'))
+
+setTimeout(() => {
+    store.dispatch(setTextFilter('bill'))
+}, 3000)
+
+// getVisibleExpenses()
+
+// console.log(store.getState())
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'))
